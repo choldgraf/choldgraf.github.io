9042814670b6eef7faed2fe167c63119d4f12247
Chris Holdgraf <choldgraf@berkeley.edu>
2020-10-10 12:18:03 -0700

    metadata

--- conf.py
+++ conf.py
@@ -59,6 +59,8 @@ html_theme_options = {
   "search_bar_position": "navbar",
 }
 
+html_favicon = "_static/favicon.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
