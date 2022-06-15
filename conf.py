38d78f11f4dd576fca3a55da31e2068bc532f33a
Chris Holdgraf <choldgraf@berkeley.edu>
2022-06-15 17:25:26 +0200

    Conf

--- conf.py
+++ conf.py
@@ -1,32 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'Predictably Noisy'
-copyright = '2020, Chris Holdgraf'
+copyright = '2022, Chris Holdgraf'
 author = 'Chris Holdgraf'
-
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "myst_nb",
     "ablog",
@@ -36,20 +15,12 @@ extensions = [
     "sphinxext.rediraffe",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "*import_posts*", "**/pandoc_ipynb/inputs/*", ".nox/*", "README.md"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'pydata_sphinx_theme'
 
 html_theme_options = {
@@ -57,14 +28,11 @@ html_theme_options = {
   "twitter_url": "https://twitter.com/choldgraf",
   "search_bar_text": "Search this site...",
   "google_analytics_id": "UA-88310237-1",
-  "navbar_end": ["search-field.html", "navbar-icon-links"],
+  "navbar_end": ["search-field.html", "theme-switcher", "navbar-icon-links"],
 }
 
 html_favicon = "_static/favicon.ico"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_title = "Predictably Noisy"
 html_static_path = ['_static']
 html_extra_path = ["feed.xml"]
 html_sidebars = {
