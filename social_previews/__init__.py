67e590d21d91613c6078ffa80baa5e6047227557
Chris Holdgraf <choldgraf@berkeley.edu>
2022-12-01 20:21:26 +0100

    Improving social preview positioning and optimizing

--- social_previews/__init__.py
+++ social_previews/__init__.py
@@ -4,17 +4,17 @@ Design principles:
 
 - Assume a two-line title, don't allow three lines, and one line should look OK.
 """
-from docutils.nodes import paragraph
 from pathlib import Path
 import matplotlib
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+from sphinxext.opengraph import get_tags
 
 matplotlib.use("agg")
 
 HERE = Path(__file__).parent
-MAX_CHAR_TAGLINE = 200
-MAX_CHAR_PAGETITLE = 53
+MAX_CHAR_description = 180
+MAX_CHAR_PAGETITLE = 65
 
 def render_page_card(app, pagename, templatename, context, doctree):
     """Create a social preview card using page metadata."""
@@ -24,20 +24,29 @@ def render_page_card(app, pagename, templatename, context, doctree):
     social_preview_config = app.config.social_preview_config or {}
     
     # Set up metadata for the card
+    tags = get_tags(app, context, doctree, app.config)
+
+    def parse_ogp_tag(tags, entry):
+        return tags.split(entry)[-1].split("content=")[1].split('/>')[0].strip().strip('"')
+    description = social_preview_config.get("description")
+    if description is None:
+        description = parse_ogp_tag(tags, "og:description")
+    pagetitle = parse_ogp_tag(tags, "og:title")
     sitetitle = context.get("docstitle", "")
-    pagetitle = context.get("title", "")
 
     if social_preview_config.get("image"):
         image = Path(app.builder.srcdir) / social_preview_config.get("image")
     else:
         image = None
 
-    # Tagline formatting
-    tagline = " ".join([ii.astext() for ii in doctree.traverse(paragraph)])
-    tagline = tagline.replace("\n\n", "\n")
-    tagline = tagline.replace("\n", "")
-    if len(tagline) > MAX_CHAR_TAGLINE:
-        tagline = tagline[:MAX_CHAR_TAGLINE].rsplit(" ", 1)[0] + "..."
+    # Grab the image shadow PNG for plotting
+    if social_preview_config.get("image_shadow"):
+        image_shadow = Path(app.builder.srcdir) / social_preview_config.get("image_shadow")
+    else:
+        image_shadow = None
+
+    # Line color at the bottom
+    line_color = social_preview_config.get("line_color")
 
     # Page title formatting
     if len(pagetitle) > MAX_CHAR_PAGETITLE:
@@ -47,13 +56,13 @@ def render_page_card(app, pagename, templatename, context, doctree):
     if not hasattr(app.env, "figure_social_card"):
         # First time this is run we need to create a bunch of Matplotlib objects
         # This is slow so we only do it once
-        fig, txt_sitetitle, txt_pagetitle, txt_tagline = create_social_card(sitetitle, pagetitle, tagline, image)
-        app.env.figure_social_card = [fig, txt_sitetitle, txt_pagetitle, txt_tagline]
+        fig, txt_sitetitle, txt_pagetitle, txt_description = create_social_card(sitetitle, pagetitle, description, image, image_shadow, line_color=line_color)
+        app.env.figure_social_card = [fig, txt_sitetitle, txt_pagetitle, txt_description]
     else:
         # The social card figure and axes already exist
         # So we just update them in order to save time
-        fig, txt_sitetitle, txt_pagetitle, txt_tagline = app.env.figure_social_card
-        update_social_card(txt_sitetitle, txt_pagetitle, txt_tagline, sitetitle, pagetitle, tagline)
+        fig, txt_sitetitle, txt_pagetitle, txt_description = app.env.figure_social_card
+        update_social_card(txt_sitetitle, txt_pagetitle, txt_description, sitetitle, pagetitle, description)
 
     # Save the image to a static directory
     path_images = "_images/social_previews"
@@ -76,10 +85,12 @@ def render_page_card(app, pagename, templatename, context, doctree):
 def create_social_card(
     site_title,
     page_title,
-    tagline,
+    description,
     image=None,
+    image_shadow=None,
     text_color="#4a4a4a",
     background_color="white",
+    line_color="#5A626B",
     font="Roboto",
     fig=None,
 ):
@@ -87,36 +98,35 @@ def create_social_card(
     ratio = 1200 / 628
     # Because Matplotlib doesn't let you specify figures in pixels, only inches
     multiple = 3
-    if fig is None:
-        fig = plt.figure(figsize=(ratio * multiple, multiple))
-        fig.set_facecolor(background_color)
+    fig = plt.figure(figsize=(ratio * multiple, multiple))
+    fig.set_facecolor(background_color)
 
-        # Text axis
-        axtext = fig.add_axes((0, 0, 1, 1))
+    # Text axis
+    axtext = fig.add_axes((0, 0, 1, 1))
 
-        # Image axis
-        ax_x, ax_y, ax_w, ax_h = (0.75, 0.73, 0.2, 0.2) 
-        axim = fig.add_axes((ax_x, ax_y, ax_w, ax_h), anchor="NE")
+    # Image axis
+    ax_x, ax_y, ax_w, ax_h = (0.68, 0.64, 0.25, 0.25) 
+    axim_logo = fig.add_axes((ax_x, ax_y, ax_w, ax_h), anchor="NE")
 
-        # Line at the bottom axis
-        axline = fig.add_axes((-.1, -.03, 1.2, .1))
-    else:
-        for ax in fig.axes:
-            ax.clear()
-        axtext, axim, axline = fig.axes
+    # Image shadow axis
+    ax_x, ax_y, ax_w, ax_h = (0.82, 0.10, 0.1, 0.1) 
+    axim_shadow = fig.add_axes((ax_x, ax_y, ax_w, ax_h), anchor="NE")
+
+    # Line at the bottom axis
+    axline = fig.add_axes((-.1, -.03, 1.2, .1))
 
     # Axes configuration
-    left_margin = 0.07
+    left_margin = 0.05
     with plt.rc_context({"font.sans-serif": [font], "text.color": text_color}):
         # Site title
         # Smaller font, just above page title
-        site_title_y_offset = 0.82
+        site_title_y_offset = 0.87
         txt_site = axtext.text(
             left_margin,
             site_title_y_offset,
             site_title,
             {
-                "size": 16,
+                "size": 14,
             },
             ha="left",
             va="top",
@@ -125,7 +135,7 @@ def create_social_card(
 
         # Page title
         # A larger font for more visibility
-        page_title_y_offset = 0.67
+        page_title_y_offset = 0.75
 
         txt_page = axtext.text(
             left_margin,
@@ -136,45 +146,50 @@ def create_social_card(
             va="top",
             wrap=True,
         )
-        txt_page._get_wrap_line_width = lambda: 430
+        txt_page._get_wrap_line_width = lambda: 400
 
-        # Tagline
+        # description
         # Just below site title, smallest font and many lines.
-        tagline_y_axis_ofset = 0.23
-        txt_tagline = axtext.text(
+        description_y_axis_ofset = 0.35
+        txt_description = axtext.text(
             left_margin,
-            tagline_y_axis_ofset,
-            tagline,
+            description_y_axis_ofset,
+            description,
             {"size": 11},
             ha="left",
-            va="bottom",
+            va="top",
             wrap=True,
         )
-        txt_tagline._get_wrap_line_width = lambda: 450
+        txt_description._get_wrap_line_width = lambda: 450
 
         # Turn off the axis so we see no ticks
         axtext.set_axis_off()
 
+    if image_shadow:
+        img = mpimg.imread(image_shadow)
+        axim_shadow.imshow(img)
+        axim_shadow.set_axis_off()
+
     # Put the logo in the top right if it exists
     if image:
         img = mpimg.imread(image)
-        axim.imshow(img)
-        axim.set_axis_off()
+        axim_logo.imshow(img)
+        axim_logo.set_axis_off()
 
     # Put a colored line at the bottom of the figure
     axline.set_axis_off()
-    axline.hlines(0, 0, 1, lw=8, color="#5A626B")
-    return fig, txt_site, txt_page, txt_tagline
+    axline.hlines(0, 0, 1, lw=8, color=line_color)
+    return fig, txt_site, txt_page, txt_description
 
 
 def update_social_card(
     ax_sitetitle,
     ax_pagetitle,
-    ax_tagline,
+    ax_description,
     site_title,
     page_title,
-    tagline,
+    description,
 ):
     ax_sitetitle.set_text(site_title)
     ax_pagetitle.set_text(page_title)
-    ax_tagline.set_text(tagline)
+    ax_description.set_text(description)
